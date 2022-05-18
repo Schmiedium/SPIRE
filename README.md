ed3b1ed3817602bc90bcfe1704023a1a0fbf1efe
Schmiedium <alexander.eisenschmied@gmail.com>
2022-05-17 20:39:03 -0400

    defined Vector3 module

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Schmiedium's Physics In Rust Engine
+
+The goal here is to learn rust and computational physics by attempting to implement
+my own program for solving physical systems 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod Vector3 {
+    pub struct Vector3 {
+        x: f64,
+        y: f64,
+        z: f64,
+    }
+
+    pub fn magnitude (vec : &Vector3) -> f64 {
+        return f64::sqrt(vec.x*vec.x + vec.y*vec.y + vec.z*vec.z);
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    
 }
